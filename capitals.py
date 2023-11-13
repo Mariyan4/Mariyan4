b322db59c9ad99201a67623ed874d61503827ad9
Mariyan Manev <145933157+Mariyan4@users.noreply.github.com>
2023-11-14 00:43:37 +0200

    Add files via upload

---
+++ capitals.py
@@ -0,0 +1,5 @@
+countries = input().split(", ")
+capitals = input().split(", ")
+final_information = {countries[index]: capitals[index] for index in range(len(capitals))}
+for country, capitals in final_information.items():
+    print(f"{country} -> {capital}")
---
+++ count_chars_in_a_string.py
@@ -0,0 +1,8 @@
+symbol = [character for character in input() if character != " "]
+letters = {}
+for symbol in symbol:
+    if symbol not in letters.keys():
+        letters[symbol] = 0
+    letters[symbol] += 1
+for symbol, occurrences in letters.items():
+    print(f"{symbol} -> {occurrences}")
